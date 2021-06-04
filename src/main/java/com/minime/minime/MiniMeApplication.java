90eebc1a2d2bbec2532430f969a36efbb5b1e13b
Ariel Abreu Silva <ariel_11_12@hotmail.com>
2021-06-04 19:04:09 -0300

    First commit

---
+++ src/main/java/com/minime/minime/MiniMeApplication.java
@@ -0,0 +1,13 @@
+package com.minime.minime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniMeApplication.class, args);
+	}
+
+}
